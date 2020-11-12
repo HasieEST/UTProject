30b44e6ba99c3b577cfdb1956afcd145323cc13a
Ayrton M <ayrtonm0@gmail.com>
2020-11-12 10:42:06 +0200

    WIP

--- GameProject.py
+++ GameProject.py
@@ -4,6 +4,14 @@ pots_deck = [2,3,4,5,6,7,8,9,10,'J','Q','K','A']
 diamonds_deck = [2,3,4,5,6,7,8,9,10,'J','Q','K','A']
 spades_deck = [2,3,4,5,6,7,8,9,10,'J','Q','K','A']
 
+#WIP
+#while playing:
+    #playing_deck = [("Ace of Hearts", 11, "pilt"),("Two of Hearts", 2, "pilt"), ("Three of Hearts", 3, "pilt"), ("Four of Hearts", 4, "pilt"), ("Five of Hearts", 5, "pilt"), ("Six of Hearts", 6, "pilt"), ("Seven of Hearts", 7, "pilt"), ("Eight of Hearts", 8, "pilt"), ("Nine of Hearts", 9, "pilt"), ("Ten of Hearts", 10, "pilt"), ("Jack of Hearts", 10, "pilt"), ("Queen of Hearts", 10, "pilt"), ("King of Hearts", 10, "pilt"),("Ace of Spades", 11, "pilt"),("Two of Spades", 2, "pilt"), ("Three of Spades", 3, "pilt"), ("Four of Spades", 4, "pilt"), ("Five of Spades", 5, "pilt"), ("Six of Spades", 6, "pilt"), ("Seven of Spades", 7, "pilt"), ("Eight of Spades", 8, "pilt"), ("Nine of Spades", 9, "pilt"), ("Ten of Spades", 10, "pilt"), ("Jack of Spades", 10, "pilt"), ("Queen of Spades", 10, "pilt"), ("King of Spades", 10, "pilt"),("Ace of Clubs", 11, "pilt"),("Two of Clubs", 2, "pilt"), ("Three of Clubs", 3, "pilt"), ("Four of Clubs", 4, "pilt"), ("Five of Clubs", 5, "pilt"), ("Six of Clubs", 6, "pilt"), ("Seven of Clubs", 7, "pilt"), ("Eight of Clubs", 8, "pilt"), ("Nine of Clubs", 9, "pilt"), ("Ten of Clubs", 10, "pilt"), ("Jack of Clubs", 10, "pilt"), ("Queen of Clubs", 10, "pilt"), ("King of Clubs", 10, "pilt"),("Ace of Diamonds", 11, "pilt"),("Two of Diamonds", 2, "pilt"), ("Three of Diamonds", 3, "pilt"), ("Four of Diamonds", 4, "pilt"), ("Five of Diamonds", 5, "pilt"), ("Six of Diamonds", 6, "pilt"), ("Seven of Diamonds", 7, "pilt"), ("Eight of Diamonds", 8, "pilt"), ("Nine of Diamonds", 9, "pilt"), ("Ten of Diamonds", 10, "pilt"), ("Jack of Diamonds", 10, "pilt"), ("Queen of Diamonds", 10, "pilt"), ("King of Diamonds", 10, "pilt")]
+    #player_cards = []
+
+
+
+
 def deal():
     hand = []
     for e in range(2): #tuleb ülevaatada.
@@ -21,8 +29,8 @@ def deal():
         if number == 3:
             random.shuffle(diamonds_deck)
             card = diamonds_deck.pop()
-            random.shuffle(diamond_deck)
-            card = diamond_deck.pop()
+            random.shuffle(diamonds_deck)
+            card = diamonds_deck.pop()
 
           
     return hand
